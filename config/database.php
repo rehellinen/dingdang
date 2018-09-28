c3bd76be3a130d09d6a6eee63971aa03df2a7432
rehellinen <912377791@qq.com>
2018-09-28 09:12:57 +0800

    数据库信息

--- config/database.php
+++ config/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'gz-cdb-jhpbcl86.sql.tencentcdb.com',
+    'hostname'        => '',
     // 数据库名
-    'database'        => 'lecture',
+    'database'        => '',
     // 用户名
-    'username'        => 'root',
+    'username'        => '',
     // 密码
-    'password'        => 'shun122526',
+    'password'        => '',
     // 端口
-    'hostport'        => '63094',
+    'hostport'        => '',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
